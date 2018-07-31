df0b7732bc6f308a5385f6b9666572f5c7496a57
chnbin <chnbin@gmail.com>
2018-07-31 19:48:27 +0800

    Add MultiSelectionExample

--- basicMultiSelectionExample/src/app/app.component.ts
+++ basicMultiSelectionExample/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,40 @@ import { Ng2SmartTableModule } from 'ng2-smart-table';
   `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  onUserRowSelect(event){
+export class AppComponent implements OnInit {
+  settings:Object;
+  data:Object;
+  public input: string = '<input type="checkbox"></input>';
+
+  constructor(private _sanitizer: DomSanitizer) {
+  this.settings = {
+    columns: {
+    username: {
+      title: 'User Name'
+    },
+    checkbox: {
+      title: 'Inactive',
+      type: 'html',
+      valuePrepareFunction: (value) => { return this._sanitizer.bypassSecurityTrustHtml(this.input); },
+      filter: false
+    }
+  }
+};
+
+this.data = [
+  {
+    username: "Bret",
+    checkbox:'yes'
+  },
+  {
+    username: "Antonette",
+    checkbox:'no'
+  }
+]; 
+}
+
+
+onUserRowSelect(event){
     console.log(event.selected)
 
     for (var i=0;i<event.selected.length;i++){
@@ -17,90 +50,5 @@ export class AppComponent {
     }
   }
 
-  settings = {
-    selectMode: 'multi',
-    columns: {
-      id: {
-        title: 'ID',
-      },
-      name: {
-        title: 'Full Name',
-      },
-      username: {
-        title: 'User Name',
-      },
-      email: {
-        title: 'Email',
-      },
-    },
-  };
 
-  data = [
-    {
-      id: 1,
-      name: 'Leanne Graham',
-      username: 'Bret',
-      email: 'Sincere@april.biz',
-    },
-    {
-      id: 2,
-      name: 'Ervin Howell',
-      username: 'Antonette',
-      email: 'Shanna@melissa.tv',
-    },
-    {
-      id: 3,
-      name: 'Clementine Bauch',
-      username: 'Samantha',
-      email: 'Nathan@yesenia.net',
-    },
-    {
-      id: 4,
-      name: 'Patricia Lebsack',
-      username: 'Karianne',
-      email: 'Julianne.OConner@kory.org',
-    },
-    {
-      id: 5,
-      name: 'Chelsey Dietrich',
-      username: 'Kamren',
-      email: 'Lucio_Hettinger@annie.ca',
-    },
-    {
-      id: 6,
-      name: 'Mrs. Dennis Schulist',
-      username: 'Leopoldo_Corkery',
-      email: 'Karley_Dach@jasper.info',
-    },
-    {
-      id: 7,
-      name: 'Kurtis Weissnat',
-      username: 'Elwyn.Skiles',
-      email: 'Telly.Hoeger@billy.biz',
-    },
-    {
-      id: 8,
-      name: 'Nicholas Runolfsdottir V',
-      username: 'Maxime_Nienow',
-      email: 'Sherwood@rosamond.me',
-    },
-    {
-      id: 9,
-      name: 'Glenna Reichert',
-      username: 'Delphine',
-      email: 'Chaim_McDermott@dana.io',
-    },
-    {
-      id: 10,
-      name: 'Clementina DuBuque',
-      username: 'Moriah.Stanton',
-      email: 'Rey.Padberg@karina.biz',
-    },
-    {
-      id: 11,
-      name: 'Nicholas DuBuque',
-      username: 'Nicholas.Stanton',
-      email: 'Rey.Padberg@rosamond.biz',
-    },
-  ];
 }
