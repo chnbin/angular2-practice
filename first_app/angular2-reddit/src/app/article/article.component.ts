bd76c0b2188e9ec04dcd21ee49936d8f1e751ac3
chnbin <chnbin@gmail.com>
2018-07-31 21:57:24 +0800

    Chapter 1: Angular2 reddit completed.

--- first_app/angular2-reddit/src/app/article/article.component.ts
+++ first_app/angular2-reddit/src/app/article/article.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article} from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host:{
+    class:'row'
+  }
 })
 export class ArticleComponent implements OnInit {
+  @Input() article: Article;
 
-  constructor() { }
+  VoteUp(): boolean{
+    this.article.votes += 1;
+    return false;
+  }
+
+  VoteDown(): boolean{
+    this.article.votes -= 1;
+    return false;
+  }
 
   ngOnInit() {
   }
---
+++ first_app/angular2-reddit/src/app/article/article.model.ts
@@ -0,0 +1,30 @@
+export class Article{
+    title :string;
+    link :string;
+    votes :number;
+
+    constructor(title:string, link:string, votes?:number){
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 0;
+    }
+
+    VoteUp(): boolean{
+        this.votes += 1;
+        return false;
+    }
+
+    VoteDown(): boolean{
+        this.votes -= 1;
+        return false;
+    }
+
+    domain():string{
+        try{
+            const link:string = this.link.split('//')[1];
+            return this.link.split('/')[0];
+        }catch(err){
+            return null;
+        }
+    }
+}
