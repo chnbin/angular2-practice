49cf94b7cfe4523327fafde23317f312057ab951
chnbin <chnbin@gmail.com>
2018-08-02 20:46:30 +0800

    Router basic complete

--- routes/basic/src/app/app.module.ts
+++ routes/basic/src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, platformBrowser } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule,
+         Routes
+       } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent} from './home/home.component';
+
+import { LocationStrategy, HashLocationStrategy } from '../../node_modules/@angular/common';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {path: '',redirectTo: 'home',pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'contactus', redirectTo: 'contact'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
+  bootstrap: [AppComponent],
 })
+
 export class AppModule { }
