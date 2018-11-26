9ae8a45b813601f10f3c1925fd70aa3598f2691e
chnbin <chnbin@gmail.com>
2018-11-26 23:45:51 +0800

    An example of Input and Output

--- section5/src/app/app.component.ts
+++ section5/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+  // ViewEncapsulation https://angular.io/api/core/ViewEncapsulation 
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+  serverElements = [{type: 'Server', name: 'Test Server', content: 'For test'}];
+
+  onServerCreated(serverData: {name: string, content: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.name,
+      content: serverData.content
+    });
+    console.log(this.serverElements);
+  }
+  onBlueprintCreate(blueprintData: {name: string, content: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.name,
+      content: blueprintData.content
+    });
+  }
 }
