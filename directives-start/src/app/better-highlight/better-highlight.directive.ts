272acdc3f204ab8a1ce60e411aa1c83ea6ab70c4
chnbin <chnbin@gmail.com>
2018-11-27 21:11:27 +0800

    A sample of HostListener

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { MockNgModuleResolver } from '@angular/compiler/testing';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -9,7 +10,24 @@ export class BetterHighlightDirective implements OnInit{
   }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
   }
 
+  @HostListener('mouseover') mouseover() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    this.renderer.setStyle(this.elRef.nativeElement, 'font-size', '1em');
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
+    this.renderer.setStyle(this.elRef.nativeElement, 'font-size', '1em');
+  }
+
+  @HostListener('click') click() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    this.renderer.setStyle(this.elRef.nativeElement, 'font-size', '2em');
+  }
 }
