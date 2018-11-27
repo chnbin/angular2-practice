180f599cc4cd892d9aab94496331416b4aa9d7f0
chnbin <chnbin@gmail.com>
2018-11-27 21:38:30 +0800

    Custom directive property

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 import { MockNgModuleResolver } from '@angular/compiler/testing';
 
 @Directive({
@@ -6,6 +6,9 @@ import { MockNgModuleResolver } from '@angular/compiler/testing';
 })
 
 export class BetterHighlightDirective implements OnInit{
+  // Dynamic setting from outside.
+  @Input() defaultColor: string = 'transparent';
+  @Input() highLightColor: string = 'red';
 
   // Bind properties of host, we can access the binded property by member variable.
   @HostBinding('style.backgroundColor') bgColor: string = 'transparent';
@@ -13,17 +16,19 @@ export class BetterHighlightDirective implements OnInit{
   @HostBinding('style.fontSize') fontSize: string = '1em';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2){
+    
   }
 
   ngOnInit() {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
+    this.bgColor = this.defaultColor;
   }
 
   @HostListener('mouseover') mouseover() {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
     // this.renderer.setStyle(this.elRef.nativeElement, 'font-size', '1em');
-    this.bgColor = 'red';
+    this.bgColor = this.highLightColor;
     this.fontColor = 'white';
     this.fontSize = '1em';
   }
@@ -32,7 +37,7 @@ export class BetterHighlightDirective implements OnInit{
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
     // this.renderer.setStyle(this.elRef.nativeElement, 'font-size', '1em');
-    this.bgColor = 'transparent';
+    this.bgColor = this.defaultColor;
     this.fontColor = 'black';
     this.fontSize = '1em';
   }
