2647efa4beb313f590a550ffec8bbec1f45bd16a
chnbin <chnbin@gmail.com>
2018-12-01 16:13:22 +0800

    Outsourcing the rounting config with ngModule

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -12,20 +11,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoute : Routes = [
-  {'path': '', 'component': HomeComponent},
-  {'path': 'users', 'component': UsersComponent, children: [
-    {'path': ':id/:name', 'component': UserComponent}
-  ]},
-  // Dynamic route
-  {'path': 'servers', 'component': ServersComponent, children:[
-    {'path': ':id', 'component': ServerComponent},
-    {'path': ':id/edit', 'component': EditServerComponent}
-  ]},
-  {'path': 'not-found', 'component': PageNotFoundComponent},
-  {'path': '**', redirectTo: '/not-found'}
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -42,7 +28,7 @@ const appRoute : Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoute)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
