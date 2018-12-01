8e5170f2d6e4ce9ec3928a91d8f49847a3cd58c5
chnbin <chnbin@gmail.com>
2018-12-01 20:12:59 +0800

    A very simple observable example

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  generatedNums: number[] = []
   constructor() { }
 
   ngOnInit() {
+    const nums = Observable.interval(100);
+    nums.subscribe(
+      (num: number) => {
+        let dice = Math.floor(Math.random() * 6) + 1;
+        console.log(`${num} generate ${dice}.`)
+        this.generatedNums.push(dice);
+        // console.log(this.generatedNums);
+      }
+    );
   }
 
 }
