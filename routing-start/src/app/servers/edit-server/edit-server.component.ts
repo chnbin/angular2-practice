a495040d77f97e13bb06ab9b696aab52b8815512
chnbin <chnbin@gmail.com>
2018-12-01 14:31:14 +0800

    Retrieving query parameters and fregment

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,9 +13,17 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private actRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    console.log(this.actRoute.snapshot.queryParams);
+    console.log(this.actRoute.snapshot.fragment);
+
+    this.actRoute.queryParams.subscribe();
+    this.actRoute.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
