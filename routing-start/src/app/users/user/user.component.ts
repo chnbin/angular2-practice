7e678145c652f246b85cc2af5e91bfe62533a07c
chnbin <chnbin@gmail.com>
2018-12-01 14:05:31 +0800

    Manually unsubscribe in ngDestory

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private activeRoute: ActivatedRoute) { }
 
@@ -20,14 +22,16 @@ export class UserComponent implements OnInit {
       name: this.activeRoute.snapshot.params['name']
     };
 
-    this.activeRoute.params.subscribe(
+    this.paramsSubscription = this.activeRoute.params.subscribe(
       (params: Params) => {
         this.user.id = params['id'];
         this.user.name = params['name'];
       }
     );
+  }
 
-
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
