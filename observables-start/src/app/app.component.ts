ba1c83c170deaaeecf18d4f1aef44ea12a87cd60
chnbin <chnbin@gmail.com>
2018-12-01 20:55:45 +0800

    Pass and recive value by Subject

--- observables-start/src/app/app.component.ts
+++ observables-start/src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  userActivated1 = false;
+  userActivated2 = false;
+
+  constructor(private usersServer: UsersService) {
+
+  }
+
+  ngOnInit() {
+    this.usersServer.userActiveated.subscribe(
+      (id: number) => {
+        if (id === 1) {
+          this.userActivated1 = true;
+        }
+
+        if (id === 2) {
+          this.userActivated2 = true;
+        }
+      }
+    );
+  }
 }
