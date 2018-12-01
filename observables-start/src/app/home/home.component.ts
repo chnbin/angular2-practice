69c9fe3676369b6d17c0e9025483ded93080180f
chnbin <chnbin@gmail.com>
2018-12-01 20:36:30 +0800

    Unsubscrible

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numSubscription: Subscription;
+  customSubscription: Subscription;
+
   generatedNums: number[] = []
   constructor() { }
 
   ngOnInit() {
     const nums = Observable.interval(100);
-    nums.subscribe(
+    this.numSubscription = nums.subscribe(
       (num: number) => {
         let dice = Math.floor(Math.random() * 6) + 1;
         console.log(`${num} generate ${dice}.`)
@@ -20,6 +23,31 @@ export class HomeComponent implements OnInit {
         // console.log(this.generatedNums);
       }
     );
+
+    const myObservable = Observable.create((observer: Observer) => {
+      setTimeout(() => {
+        observer.next('1st package');
+      },2000);
+
+      setTimeout(() => {
+        observer.next('2nd package');
+      }, 4000);
+
+      setTimeout(() => {
+        observer.error('This dose error');
+      },5000);
+    });
+
+    this.customSubscription = myObservable.subscribe(
+      (data: string) => {console.log(data);},
+      (error: string) => {console.log(error);},
+      () => { console.log('Complete');}
+    );
+  }
+
+  ngOnDestroy() {
+    this.numSubscription.unsubscribe();
+    this.customSubscription.unsubscribe();
   }
 
 }
