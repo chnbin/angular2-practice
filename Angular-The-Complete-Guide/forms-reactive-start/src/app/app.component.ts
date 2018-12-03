fed493076d2954d9d9fb458f3f9dc69b15cdcf7f
chnbin <chnbin@gmail.com>
2018-12-03 22:19:08 +0800

    Customerizing validation rule and using error codes

--- Angular-The-Complete-Guide/forms-reactive-start/src/app/app.component.ts
+++ Angular-The-Complete-Guide/forms-reactive-start/src/app/app.component.ts
@@ -9,12 +9,13 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Admin', 'test'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       // Group username and email into userdata
       'userdata': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -30,5 +31,16 @@ export class AppComponent implements OnInit{
     const control = new FormControl(null, Validators.required);
     // Explicit cast or error
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // Return a javascript object: key is string type; value is boolean type.
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      // This will be stored in username's error object.
+      return {'nameIsForbidden': true};
+    }
+
+    // This is very important, if validation is successful you have to return null or nothing.
+    return null;
   }
 }
