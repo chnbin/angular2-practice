634478b5cb08dffbad72d105b03f4ea83efb10c9
chnbin <chnbin@gmail.com>
2018-12-10 22:23:42 +0800

    Add http-start

---
+++ Angular-The-Complete-Guide/http-start/src/app/servers.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+
+@Injectable()
+export class ServersService {
+  constructor(private http: HttpClient) {
+
+  }
+}
